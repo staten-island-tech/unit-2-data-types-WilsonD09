bb6cf8e6c63bdebd1a4c56193d94299253481182
WilsonD09 <158211754+WilsonD09@users.noreply.github.com>
2024-02-06 13:58:31 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,24 @@
-x = 3
-if x : [3]
-    print('odd')
-else:
+""" Challenge 1 
+x = float(input("give me a number"))
+if (x%2) == 0:
     print('even')
+else:
+    print('odd') """
+
+""" Challenge 2
+x = int(input('Subtotal'))
+Service = input('How was the service?')
+if Service == ('bad'):
+    print(float(x))
+elif Service == ('okay'):
+    print(float(x*1.15))
+elif Service == ('good'):
+    print(float(x*1.2))
+elif Service == ('great'):
+    print(float(x*1.25)) """
+
+
+def factors(number):
+    factorslist = []
+    for i in range(1,number+1):
+        
